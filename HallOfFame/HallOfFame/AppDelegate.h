72427f750fe994ba90dc9a0d0ceba32885bbcc5d
jd.huaxiaochun <huaxiaochun@jd.com>
2016-06-30 11:53:20 +0800

    Pod install

---
+++ HallOfFame/HallOfFame/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HallOfFame
+//
+//  Created by jd.huaxiaochun on 16/6/30.
+//  Copyright © 2016年 jd.slipknot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HallOfFame/HallOfFame/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HallOfFame
+//
+//  Created by jd.huaxiaochun on 16/6/30.
+//  Copyright © 2016年 jd.slipknot. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
